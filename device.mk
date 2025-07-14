bc36826d5f05333e0ba17524117eb348a72eff65
Ghosuto <clash.raja10@gmail.com>
2025-07-14 16:52:30 +0000

    marble: Include app debloater
    - Let's remove some usless apps
    
    Signed-off-by: AnilShebin <anilshebin@gmail.com>

--- device.mk
+++ device.mk
@@ -62,6 +62,10 @@ PRODUCT_PACKAGES += \
     WifiResMarble \
     WifiResMarbleCN
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
