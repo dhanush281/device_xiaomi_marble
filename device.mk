fff3da701ceba790c6ef59be7ad57761c760ea71
dhanush281 <dhanushvozh345@gmail.com>
2026-06-19 07:07:40 +0000

    marble:marble:device specs added!

--- device.mk
+++ device.mk
@@ -58,6 +58,7 @@ PRODUCT_PACKAGES += \
     NfcResMarble \
     SettingsProviderResMarble \
     SettingsProviderResMarbleCN \
+    SettingsResMarble \
     SystemUIResMarble \
     WifiResMarble \
     WifiResMarbleCN
