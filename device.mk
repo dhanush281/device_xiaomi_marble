5bb1597f86034583d50ce61802aa2f903703ec60
Dhanush /.\./. <106550362+dhanush281@users.noreply.github.com>
2026-06-19 13:19:23 +0530

    Configure refresh rates for SurfaceFlinger
    Add refresh rate configuration for SurfaceFlinger.

--- device.mk
+++ device.mk
@@ -85,3 +85,7 @@ PRODUCT_COPY_FILES += \
 # Vibrator
 $(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.xiaomi)
 $(call soong_config_set_bool,qti_vibrator,use_effect_stream,true)
+
+# refresh rate
+$(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
+$(call soong_config_set,surfaceflinger,frame_rate_category_min,60)
