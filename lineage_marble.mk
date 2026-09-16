e5d97f177c2aa41b87b698e7bf4233b7d750a0d9
dhanush281 <dhanushvozh345@gmail.com>
2026-09-16 14:52:22 +0530

    Marble:Bringup for RisingOS-Revived

--- lineage_marble.mk
+++ lineage_marble.mk
@@ -24,29 +24,18 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Lunaris Official
-LUNARIS_BUILD_TYPE := OFFICIAL
-TARGET_OPTIMIZED_DEXOPT := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_DISABLE_EPPE := true
-WITH_BCR := true
-TARGET_CUSTOM_UDFPS := false
+RISING_MAINTAINER="Dhanush & Ashwani"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Snapdragon 7+gen2" \
+    RisingMaintainer="Dhanush & Ashwani"
+RISING_MAINTAINER := Dhanush & Ashwani
+TARGET_ENABLE_BLUR := true
+PRODUCT_NO_CAMERA := true
+
+# CORE build flags
 WITH_GMS := true
-TARGET_SUPPORTS_BLUR := true
-TARGET_DEFAULT_PIXEL_LAUNCHER := false
-TARGET_SUPPORTS_QUICK_TAP := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-USE_REALITY_ENGINE := true
-TORCH_STR_SUPPORTED := true
-USE_ADVANCED_DISPLAY_COLOR := false
+TARGET_USES_PICO_GAPPS := true
 
-# Enable vulkan
-USE_DEFAULT_VULKAN := true
-
-# For old devices add this also
-# Vulkan video issue
-TARGET_NEEDS_VULKAN_MEDIA_FIX := true
-
-# refresh rate list (optional) - defining this flags makes the controller skip the parsing of Display.Mode refresh rates
-# for supported refresh rates. The refresh rate controller will also assume that all the defined refresh rates are "supported"
-TARGET_SUPPORTED_REFRESH_RATES := 60,90,120
+# VANILLA build with MICROG
+#WITH_GMS := false
+#WITH_MICROG := true
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,17 @@
 echo 'Hello! we need to clone stuffs needed for your device, please wait tll process complete'
 
 echo 'Cloning Device Common Tree'
-	git clone https://github.com/dhanush281/device_xiaomi_sm8450-common.git device/xiaomi/sm8450-common
+	git clone https://github.com/dhanush281/device_xiaomi_sm8450-common.git -b 17 device/xiaomi/sm8450-common
 
 echo "Cloning MemeCamera Tree"
 git clone https://github.com/dhanush281/device_xiaomi_miuicamera-marble device/xiaomi/miuicamera-marble
 git clone https://github.com/dhanush281/vendor_xiaomi_miuicamera-marble vendor/xiaomi/miuicamera-marble
 
 echo 'Cloning Hardware Dolby Tree'
-    git clone https://github.com/dhanush281/android_hardware_dolby.git hardware/dolby
+    git clone https://github.com/dhanush281/android_hardware_dolby.git -b 17 hardware/dolby
 
 echo 'Cloning Hardware Xiaomi Tree'
-	git clone https://github.com/dhanush281/android_hardware_xiaomi.git hardware/xiaomi
+	git clone https://github.com/dhanush281/android_hardware_xiaomi.git -b 17 hardware/xiaomi
 
 echo 'Cloning Kernel Tree'
     git clone https://github.com/dhanush281/android_kernel_xiaomi_sm8450.git kernel/xiaomi/sm8450
@@ -19,13 +19,10 @@ echo 'Cloning Kernel Tree'
     git clone https://github.com/dhanush281/android_kernel_xiaomi_sm8450-modules.git kernel/xiaomi/sm8450-modules
 
 echo 'Cloning Vendor Tree'
-    git clone https://github.com/dhanush281/vendor_xiaomi_marble vendor/xiaomi/marble
+    git clone https://github.com/dhanush281/vendor_xiaomi_marble -b 17 vendor/xiaomi/marble
 
 echo 'Cloning Vendor Common Tree'
-    git clone https://github.com/dhanush281/vendor_xiaomi_sm8450-common.git vendor/xiaomi/sm8450-common
-
-echo 'Signing'
-    git clone https://github.com/Lunaris-AOSP/vendor_lunaris-priv_keys vendor/lunaris-priv/keys
+    git clone https://github.com/dhanush281/vendor_xiaomi_sm8450-common.git -b 17 vendor/xiaomi/sm8450-common
 
 echo 'Firmware'
 	git clone https://github.com/dhanush281/vendor_xiaomi_marble-firmware vendor/xiaomi/marble-firmware
